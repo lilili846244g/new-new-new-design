a5ef9731a076757fe4f0b3bc3ec594e4da1d5705
ly li  <lilili846244@gmail.com>
2023-02-10 00:03:05 +0800

    css updated

--- js/new.js
+++ js/new.js
@@ -4,14 +4,13 @@ gsap.utils.toArray(".ts-hero__sections").forEach(function (elem) {
   var color = elem.getAttribute("data-color");
   ScrollTrigger.create({
     trigger: elem,
-    start: "top 50%",
-    end: "bottom 80%",
-    onEnter: () => gsap.to(".section-main", { backgroundColor: color }),
-    onLeave: () =>
-      gsap.to(".section-main", { backgroundColor: "rgba(38, 38, 38, 1)" }),
+    start: "top bottom",
+    end: "bottom 95%",
+    onEnter: () => gsap.to(".bkg-an", { backgroundColor: color }),
+    onLeave: () => gsap.to(".bkg-an", { backgroundColor: rgba(38, 38, 38, 1) }),
     onLeaveBack: () =>
-      gsap.to(".section-main", { backgroundColor: rgb(248, 238, 238) }),
-    onEnterBack: () => gsap.to(".section-main", { backgroundColor: color }),
+      gsap.to(".bkg-an", { backgroundColor: rgba(38, 38, 38, 1) }),
+    onEnterBack: () => gsap.to(".bkg-an", { backgroundColor: color }),
     markers: true,
   });
 });
